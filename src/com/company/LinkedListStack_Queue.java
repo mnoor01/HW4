6c9642224c0b9367257b6e70a0f45e20b5ebb9c4
Melina Gonzalez <melinag1992@gmail.com>
2017-10-15 15:31:23 -0400

    intial homework 4 commit

---
+++ src/com/company/LinkedListStack_Queue.java
@@ -0,0 +1,48 @@
+package com.company;
+import java.util.*;
+public class LinkedListStack_Queue {
+
+    Scanner scan= new Scanner(System.in);
+    private LinkedList <String> stack_Queue= new LinkedList<>();
+    private String tempRemoveCopy;
+    private String tempAddCopy;
+    public void push() {
+        System.out.println("enter String: ");
+        String userIn = scan.nextLine();
+
+    }
+    public  void setAddCopy(String userIn) {
+        this.tempAddCopy= userIn;
+    }
+    public void copy() {stack_Queue.add(tempAddCopy);}
+    public void setTempRemoveCopy(String userIn){
+        this.tempRemoveCopy=userIn;
+    }
+    public void remove(){
+        if(stack_Queue.size()>=10) {
+            System.out.println("you list is too big make it smaller.");
+        }
+        else {
+            try {
+                System.out.println("remove string");
+                setTempRemoveCopy(stack_Queue.getLast());
+                stack_Queue.removeLast();
+            } catch (NoSuchElementException m)
+            {
+                System.out.println("Empty list please add string first.");
+            }
+        }
+    }
+    public void undo() {
+        if(!stack_Queue.isEmpty() || tempRemoveCopy != ""){
+            stack_Queue.add(tempRemoveCopy);
+        }
+        else {
+            System.out.println("empty list please add string first.");
+        }
+    }
+    public void printList() {
+        System.out.println(stack_Queue);
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+public static LinkedListStack_Queue nuevoList= new LinkedListStack_Queue();
+    public static void main(String[] args) {
+        nuevoList.printList();
+        options();
+
+	// write your code here
+    }
+    public static void options() {
+        int quitLoop= 0;
+        do {
+
+            System.out.println("enter command:");
+            System.out.println("options");
+            System.out.println("Type\"add\" to input String to list.");
+            System.out.println("Type\"copy\" to re enter last string.");
+            System.out.println("type\"delete\" to remove last string.");
+            System.out.println("Type\"undo\" to add back removed string.");
+            System.out.println("Type\"quit\" to exit program.");
+
+            Scanner scan= new Scanner(System.in);
+            String command= scan.nextLine();
+            command= command.toUpperCase();
+
+            switch(command) {
+                case "ADD":
+                    nuevoList.push();
+                    break;
+                case "COPY":
+                    nuevoList.copy();
+                case"DELETE":
+                    nuevoList.remove();
+                    break;
+                case"UNDO":
+                    nuevoList.undo();
+                    break;
+                case"QUIT":
+                    System.out.println("Ending process.");
+                    quitLoop++;
+                    break;
+                    default:
+                        System.out.println("Enter valid number.");
+            }
+
+        }while (quitLoop==0);
+    }
+}
